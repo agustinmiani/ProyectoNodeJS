f07995f51f626eed283c2b2dabb8214e4c5b542f
agustinmiani <agustinmiani@gmail.com>
2016-12-02 17:22:20 -0300

    nuevos cambios servidor 2

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
-var fs = require('fs');
 var http = require("http");
-var server = http.createServer();
 var url = require("url");
+var noticiasService = require('./noticias');
+var mu = require('mu2');
+var server = http.createServer();
+var fs = require('fs');
 
 
-server.on("request", function(req,res){
-	var urlData=url.parse(req.url);
+mu.root = __dirname + '/templates';
+// mu.root = __dirname;
+
+var page = {};
+
+server.on ("request", function (req, res) {
+
+	var urlData = url.parse(req.url, true);
 	var path = urlData.pathname;
-	var filePath= "public"+path;
-	fs.exists(filePath,function(exists){
-		if(exists){
-			fs.readFile(filePath, function(err,data){
-				if (err){
-					res.writeHead(500);
-					if (path=="/"){
-						filePath = "public/index.html";
-						fs.readFile(filePath, function(err,data){
-							res.end(data);
-						})
+
+	// if(path == '/' || path == '/noticias') {
+	if(path == '/noticias') {
+		//WORK WITH MUSTACHE
+
+		mu.clearCache();
+
+		page.title = 'Infobae - Curso - Dinamico';
+		page.description = '';
+		var noticias = noticiasService.get();
+		// var stream = mu.compileAndRender('index.html', {page: page, nombre: "Jonathan", noticias: noticiasService.get()});
+		var stream = mu.compileAndRender('index.html', {title: "FUNCIONA!", noticias: noticias});
+
+		stream.pipe(res);
+	}
+	else{
+		//Trabajo como antes
+
+		var filePath= "public"+path;
+		fs.exists(filePath,function(exists) {
+			if (exists) {
+				fs.readFile(filePath, function (err, data) {
+					if (err) {
+						res.writeHead(500);
+						if (path == "/") {
+							filePath = "templates/index.html";
+							fs.readFile(filePath, function (err, data) {
+								res.end(data);
+							})
+						}
+					} else {
+						res.end(data);
 					}
-				}else{
-					res.end(data);
-				}
-			})
-		}else {
-			res.writeHead(404);
-			res.end("No existe")
-		}
-	});
+				})
+			} else {
+				res.writeHead(404);
+				res.end("No existe");
+			}
+		})
+	}
 });
+
 
 server.listen(process.env.PORT || 3000);
--- noticias.js
+++ noticias.js
@@ -1,3 +1,22 @@
-/**
- * Created by amiani on 12/2/16.
- */
+module.exports = {
+    get: function(){
+        var articulos = [];
+        var nuevo = new Articulo(0,"Chapecoense","Gana la copa", true, "aca tiro fruta");
+        var nuevo1 = new Articulo(1,"Manu","Compra su Quemador de grasas", false, "aca tiro fruta");
+        var nuevo2 = new Articulo(2,"Manu","Le pega a todos por locura de la pill", true, "aca tiro fruta");
+        articulos.push(nuevo);
+        articulos.push(nuevo1);
+        articulos.push(nuevo2);
+
+        return articulos;
+    },
+};
+
+
+var Articulo = function (id,titulo, contenido, link, extra) {
+    this.id = id;
+    this.titulo = titulo;
+    this.contenido = contenido;
+    this.link = link;
+    this.extra = extra;
+};
